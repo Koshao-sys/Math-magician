f5ad1a38c6ba9956862c199aace35c47b7a6e9bd
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-03-22 09:51:12 +0300

    Add calculator elements

---
+++ src/components/Calculator.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import './Calculator.css';
+
+function Display() {
+  return (
+    <div className="DisplayArea">
+      <p>0</p>
+    </div>
+  );
+}
+
+function KeyArea() {
+  const Items = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+  return (
+    <div className="key-area">
+      {Items.map((Item) => (
+        <button key={uuidv4()} type="submit" className="key-btn">{Item}</button>
+      ))}
+    </div>
+  );
+}
+
+function Calculator() {
+  return (
+    <div className="Calculator">
+      <Display />
+      <KeyArea />
+    </div>
+  );
+}
+
+export default Calculator;
