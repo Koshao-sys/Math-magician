a3360c3f61bbac8f14395de190ef1b3130dc0370
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-03-23 18:40:57 +0300

    Add functionality to enable math operations

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import './Calculator.css';
-import Display from './DisplayCalc';
 import KeyArea from './KeyArea';
 
 function Calculator() {
   return (
-    <div className="Calculator">
-      <Display />
-      <KeyArea />
-    </div>
+    <KeyArea />
   );
 }
 
